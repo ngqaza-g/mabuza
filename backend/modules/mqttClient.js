2dfc7fb27bc019353105fe18ca4fcb174eb9efb4
Ngqabutho Dlamini <ngqaza.dlamz47@gmail.com>
2023-04-26 10:04:49 -0700

    Working on facial recognition

--- backend/modules/mqttClient.js
+++ backend/modules/mqttClient.js
@@ -1,13 +1,14 @@
 const mqtt = require('mqtt');
 const Vehicle = require('../models/Vehicle');
+const recogniseFace = require('./recognise_face');
 
 const mqttClient = mqtt.connect('mqtt://localhost');
-// console.log(mqttClient)
 
 
 mqttClient.on('connect', ()=>{
     console.log("Connected to an MQTT broker");
-    mqttClient.subscribe("fingerprint_id")
+    mqttClient.subscribe("fingerprint_id");
+    mqttClient.subscribe("recognise_face");
 });
 
 
@@ -15,6 +16,12 @@ mqttClient.on('message', (topic, message)=>{
     if(topic === "fingerprint_id"){
         const { driver_id, fingerprint_id, license_plate_number } = JSON.parse(message.toString());
         console.log(driver_id);
+    }
+
+    if(topic === "recognise_face"){
+        const image = message.toString();
+        const result = recogniseFace(image);
+        console.log(result);
     }
 })
 
